08a4b2747c1b5d5e9dc4db51ca812e54451e1e52
Amarildo Dos Santos <amdos-sa@c1r9s1.42luanda.com>
2025-03-13 11:53:15 +0100

    l01 - encapsulamento

--- l01/main.cpp
+++ l01/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream> 
-
+// Encapsulation
 class   Employe{
-public:
+private:
     std::string Name;
     std::string Company;
     int Age;
+public:
+    void    setName(std::string name){
+        Name = name;
+    }
+    std::string    getName(){
+        return (Name);
+    }
+    void    setCompany(std::string company){
+        Company = company;
+    }
+    std::string    getCompany(){
+        return (Company);
+    }
+    void    setAge(int age){
+        if (age >= 18)
+            Age = age;
+        else
+            std::cout << "Error\n";
+    }
+    int    getAge(){
+        return (Age);
+    }
     void    IntriduceYourSelf()
     {
         std::cout << " Name -> " << Name << std::endl;
@@ -26,4 +48,7 @@ int main()
 
     Employe employe2 = Employe("#####", "Segredo", 19);
     employe2.IntriduceYourSelf();
+
+    employe1.setAge(16);
+    std::cout << employe1.getName() << " is " << employe1.getAge() << " and works in " << employe1.getCompany() <<  "\n";
 }
